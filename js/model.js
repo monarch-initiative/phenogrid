156ad66dbbb17df2a56adc43a96a8972f5e73f80
SC <github.com@genkisugi.net>
2015-06-14 16:51:52 -0700

    add jsdoc

--- js/model.js
+++ js/model.js
@@ -1,20 +1,44 @@
-
-
+/** 
+ * Simple data point.
+ * 
+ * @see module:phenogrid
+ * @module model
+ */
 module.exports = {
 
-
+    /**
+     * Data point.
+     * 
+     * @constructor
+     * @param {Number} x - x cordinate.
+     * @param {Number} y - y cordinate.
+     * @returns {this} new instance
+     */
     modelDataPoint: function(x,y) {
 	this.xID = x;
 	this.yID = y;
     },
 
-    // Makes sure that matches are when both the X & Y values are the same
+    /**
+     * Makes sure that matches are when both the X & Y values are the
+     * same.
+     * 
+     * @static
+     * @param {modelDataPoint} point1 - A point.
+     * @param {modelDataPoint} point2 - A point.
+     * @returns {Boolean} data equality
+     */
     modelDataPointEquals: function(point1,point2) {
 	return point1.xID === point2.xID && point1.yID === point2.yID;
     },
 
-    
-    // Prints the point in a easy to understand way
+    /**
+     * Prints the point in an easy to understand way.
+     * 
+     * @static
+     * @param {modelDataPoint} point1 - A point.
+     * @returns {String} printable version on point.
+     */
     modelDataPointPrint: function(point) {
 	return "X:" + point.xID + ", Y:" + point.yID;
     }
