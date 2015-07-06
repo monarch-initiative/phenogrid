efa756f22390e73e6fe5f6995acc9905419a17b4
Zhou Yuan <yuanzhou19@gmail.com>
2015-07-06 16:14:34 -0400

    removed debugging code for _capitalizeString()

--- js/phenogrid.js
+++ js/phenogrid.js
@@ -2036,8 +2036,6 @@ var TooltipRender = require('./render.js');
 		_capitalizeString: function(word) {
 			if (word === undefined) {
 				return "Undefined";
-			} else if (word === null) {
-				return " ";
 			} else {
 				return word.charAt(0).toUpperCase() + word.slice(1);
 			}
