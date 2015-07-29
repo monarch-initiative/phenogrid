20d90bcb91d8b50cf9c2dab01313e739fe9e3fef
kshefchek <kshefchek@gmail.com>
2015-07-29 13:16:12 -0700

    change jQuery to dollar sign

--- js/phenogrid.js
+++ js/phenogrid.js
@@ -1586,7 +1586,7 @@ var TooltipRender = require('./tooltiprender.js');
         _ajaxPostData: function (target, url, postData) {
             var self = this;
             var res;
-            jQuery.ajax({
+            $.ajax({
                 url: url,
                 method: 'POST',
                 data: postData,
