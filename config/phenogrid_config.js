19a98e5561b88315cabf2dd3c34e843e6119ddbb
Zhou Yuan <yuanzhou19@gmail.com>
2015-09-17 17:17:47 -0400

    Removed unused variables and formated config

--- config/phenogrid_config.js
+++ config/phenogrid_config.js
@@ -1,14 +1,14 @@
 var configoptions = {
-    "serverURL": "",
-    "selectedCalculation": "0",
-    "invertAxis": false,
-    "selectedSort": "Frequency",
-    "targetGroupList" : [
-		{"name": "Homo sapiens", "taxon": "9606","crossComparisonView": true, "active": true},
-		{"name": "Mus musculus", "taxon": "10090", "crossComparisonView": true, "active": true},
-		{"name": "Danio rerio", "taxon": "7955", "crossComparisonView": true, "active": true},
-        {"name": "Drosophila melanogaster", "taxon": "7227", "crossComparisonView": false, "active": false},
-        {"name": "UDPICS", "taxon": "UDPICS", "crossComparisonView": false, "active": false}
-	   ]
-    };
+    serverURL: "http://monarchinitiative.org",
+    selectedCalculation: 0,
+    invertAxis: false,
+    selectedSort: "Frequency",
+    targetGroupList: [
+        {name: "Homo sapiens", taxon: "9606",crossComparisonView: true, active: true},
+        {name: "Mus musculus", taxon: "10090", crossComparisonView: true, active: true},
+        {name: "Danio rerio", taxon: "7955", crossComparisonView: true, active: true},
+        {name: "Drosophila melanogaster", taxon: "7227", crossComparisonView: false, active: false},
+        {name: "UDPICS", taxon: "UDPICS", crossComparisonView: false, active: false}
+    ]
+};
          
