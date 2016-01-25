569a18626ec387cafb5c73697a8cbaa7b8c58685
Zhou Yuan <yuanzhou19@gmail.com>
2016-01-25 10:43:59 -0500

    Synced with master

---
+++ config/phenogrid_config.js
@@ -0,0 +1,14 @@
+var configoptions = {
+    serverURL: "http://monarchinitiative.org",
+    selectedCalculation: 0,
+    selectedSort: "Frequency",
+    targetGroupList: [
+        {name: "Homo sapiens", taxon: "9606",crossComparisonView: true, active: true},
+        {name: "Mus musculus", taxon: "10090", crossComparisonView: true, active: true},
+        {name: "Danio rerio", taxon: "7955", crossComparisonView: true, active: true},
+        {name: "Drosophila melanogaster", taxon: "7227", crossComparisonView: false, active: false},
+        {name: "Caenorhabditis elegans", taxon: "6239", crossComparisonView: false, active: false},
+        {name: "UDPICS", taxon: "UDPICS", crossComparisonView: false, active: false}
+    ]
+};
+         
